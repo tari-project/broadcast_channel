40c131d1ffe82d3501a8ce209003e3f786ba4892
Cayle Sharrock <CjS77@users.noreply.github.com>
2022-06-07 11:22:17 +0100

    chore: update rustfmt.toml (#6)
    Brings linting rules in line with the main Tari repo
    We aso run cargo fmt to update the code to the new rules

--- benches/channel.rs
+++ benches/channel.rs
@@ -20,9 +20,10 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use std::{iter::repeat, time::Duration};
+
 use criterion::{criterion_group, Criterion};
 use futures::{future::join_all, SinkExt, StreamExt};
-use std::{iter::repeat, time::Duration};
 use tari_broadcast_channel::{bounded, raw_bounded};
 use tokio::runtime::Runtime;
 
